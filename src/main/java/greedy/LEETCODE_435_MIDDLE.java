6dfc43f8a4d98d70ddcb470deffbeb5cc76b72e1
zmc <m18996248458@163.com>
2021-05-18 16:44:29 +0800

    test commit

---
+++ src/main/java/greedy/LEETCODE_435_MIDDLE.java
@@ -0,0 +1,17 @@
+package greedy;
+
+/**
+ * @description:
+ * @author: ZMC
+ * @email: m18996248458@163.com
+ * @create: 2021-05-18 16:40
+ */
+public class LEETCODE_435_MIDDLE {
+    public static void main(String[] args) {
+
+    }
+
+    public static int eraseOverlapIntervals(int[][] intervals) {
+
+    }
+}
