e7aae2b0819b71dc9707db7009dffe4d7543117a
zmc <m18996248458@163.com>
2021-05-18 18:57:26 +0800

    add greedy 767

---
+++ src/main/java/greedy/LEETCODE_767_MIDDLE.java
@@ -0,0 +1,15 @@
+package greedy;
+
+/**
+ * @description:
+ * @author: ZMC
+ * @email: m18996248458@163.com
+ * @create: 2021-05-18 18:53
+ */
+public class LEETCODE_767_MIDDLE {
+    public static void main(String[] args) {
+
+    }
+
+
+}
